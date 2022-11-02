362cacb5b35a49435ada4e18a23ff5ec3e741b35
humaiun <humaiunkobirr@gmail.com>
2022-11-02 22:17:47 +0600

    added header

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../../images/logo/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="menu-items">
+      <Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
+        <Container>
+          <Navbar.Brand className="logo" href="#home">
+            <p>The</p>
+            <img src={logo} alt="" />
+            <p>Doctor</p>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
